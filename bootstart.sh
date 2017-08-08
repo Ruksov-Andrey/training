90932273295efe094f79a43a69e08018ff5dd3a7
Ruksov-Andrey <theruksov@gmail.com>
2017-08-08 21:46:36 +0300

    switch on task1-1

--- bootstart.sh
+++ bootstart.sh
@@ -3,5 +3,5 @@
 yum install git -y
 git clone https://github.com/Ruksov-Andrey/training/
 cd /home/vagrant/training
-git checkout task1
+git checkout task1-1
 sed '3!d' /home/vagrant/training/README.txt
