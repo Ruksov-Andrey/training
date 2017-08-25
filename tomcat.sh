bda550d3387fd316b75f1054e4c35ff2f1dfe490
Ruksov-Andrey <30682179+Ruksov-Andrey@users.noreply.github.com>
2017-08-25 10:55:48 +0300

    gradle file

--- tomcat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-	echo "Setup TOMCAT"
-	yum install java-1.8.0-openjdk -y
-	yum install tomcat tomcat-webapps tomcat-admin-webapps -y
-	systemctl enable tomcat
-	systemctl start tomcat 
