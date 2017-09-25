0400b10450c3e9635fedfddfadb0b929821c7af0
Ruksov-Andrey <30682179+Ruksov-Andrey@users.noreply.github.com>
2017-09-25 11:54:40 +0300

    new change

--- cookbooks/test1/recipes/default.rb
+++ cookbooks/test1/recipes/default.rb
@@ -5,26 +5,15 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
-directory '/etc/docker' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-file '/etc/docker/daemon.json' do
-  content '{"insecure-registries" : ["172.20.20.12:5000"]}'
-  mode '0755'
-  owner 'root'
-  group 'root'
-end
-
-
-
 
 docker_service 'default' do
   action [:create, :start]
 end
 
+docker_service 'default' do
+  insecure_registry '{"insecure-registries" : ["172.20.20.12:5000"]}'
+end
 
-
+docker_service 'default' do
+  action [:restart]
+end
